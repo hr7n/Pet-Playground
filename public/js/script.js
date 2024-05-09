059e50ce256212293732cad36c5a0c70ab8568a8
mhorton <horton-michael@users.noreply.github.com>
2024-05-09 12:26:22 -0400

    fixes like button bug

--- public/js/script.js
+++ public/js/script.js
@@ -3,41 +3,40 @@ const likeHandler = async (event) => {
   const likeBtn = event.target;
   const likeDisplay = likeBtn.parentElement.lastElementChild;
   let { id, likes: likeCount } = event.target.dataset;
-  console.log("like buttons?");
+  console.log('like buttons?');
   //   this is if the comment is unliked currently then a click will like and update like
-  if (likeBtn.getAttribute("data-liked") === "false") {
-    // This is an "like" action
+  if (likeBtn.getAttribute('data-liked') === 'false') {
     likeCount++;
-    likeBtn.setAttribute("style", "background-color:black;");
-    likeBtn.setAttribute("data-liked", "true");
-    // ...
+    likeBtn.classList.add('btn-dark;');
+    likeBtn.setAttribute('data-liked', 'true');
   } else {
-    // This is an "unlike" action
     likeCount--;
-    likeBtn.setAttribute("style", "background-color:white;");
-    likeBtn.setAttribute("data-liked", "false");
+    likeBtn.classList.add('btn-dark');
+    likeBtn.setAttribute('data-liked', 'false');
     // ...
   }
 
   const response = await fetch(`/api/pet-post/${id}`, {
-    method: "PUT",
+    method: 'PUT',
     body: JSON.stringify({
       likes: likeCount,
     }),
-    headers: { "Content-Type": "application/json" },
+    headers: { 'Content-Type': 'application/json' },
   });
 
-  //     //fetch to update db if success then
+  //fetch to update db if success then
   if (response.ok) {
-    likeBtn.setAttribute("data-likes", likeCount);
-    likeDisplay.textContent = "Likes: " + likeCount;
+    likeBtn.setAttribute('data-likes', likeCount);
+    likeDisplay.textContent = '❤️ Likes: ' + likeCount;
   } else {
-    alert("Failed to like post");
+    alert('Failed to like post');
   }
 };
 
-const likeButtons = document.querySelectorAll(".like-button");
-
-for (const button of likeButtons) {
-  button.addEventListener("click", likeHandler);
-}
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.addEventListener('click', function (event) {
+    if (event.target.matches('.like-button')) {
+      likeHandler(event);
+    }
+  });
+});
