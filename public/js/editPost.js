1c475acd87a0d3c8fc6926e32a64379204803de0
mhorton <horton-michael@users.noreply.github.com>
2024-05-10 14:00:28 -0400

    removes title

--- public/js/editPost.js
+++ public/js/editPost.js
@@ -2,20 +2,17 @@ const editPostFormHandler = async (event) => {
   event.preventDefault();
 
   const postid = event.target.dataset.postid;
-  console.log(postid);
-  const title = document.querySelector(`#title${postid}`).value.trim();
   const caption = document.querySelector(`#caption${postid}`).value.trim();
   const petUserName = location.href.substring(
-    location.href.lastIndexOf("/") + 1
+    location.href.lastIndexOf('/') + 1
   );
 
   const response = await fetch(`/api/pet-post/${postid}`, {
-    method: "PUT",
+    method: 'PUT',
     body: JSON.stringify({
-      title,
       caption,
     }),
-    headers: { "Content-Type": "application/json" },
+    headers: { 'Content-Type': 'application/json' },
   });
 
   console.log(response);
@@ -23,10 +20,10 @@ const editPostFormHandler = async (event) => {
   if (response.ok) {
     document.location.replace(`/profile/${petUserName}`);
   } else {
-    alert("Failed to edit Post");
+    alert('Failed to edit Post');
   }
 };
-const submitEditBtns = document.querySelectorAll(".editSubmitBtn");
+const submitEditBtns = document.querySelectorAll('.editSubmitBtn');
 for (const submitEditBtn of submitEditBtns) {
-  submitEditBtn.addEventListener("click", editPostFormHandler);
+  submitEditBtn.addEventListener('click', editPostFormHandler);
 }
