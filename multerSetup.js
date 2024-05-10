91cad3077325d91e752e1990f171082068183f24
mhorton <horton-michael@users.noreply.github.com>
2024-05-10 18:54:44 -0400

    initializes AWS, multer-s3

--- multerSetup.js
+++ multerSetup.js
@@ -1,8 +1,18 @@
-const multer = require("multer");
+const AWS = require('aws-sdk');
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+const s3 = new AWS.S3();
 
 const storagePosts = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/images/uploads/");
+    cb(null, './public/images/uploads/');
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
@@ -11,7 +21,7 @@ const storagePosts = multer.diskStorage({
 
 const storagePets = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/images/pet_pics/");
+    cb(null, './public/images/pet_pics/');
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
